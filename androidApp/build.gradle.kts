999c8079e1cee487ba3ec874db1a77503ec25bec
weijia <richardwangwang@gmail.com>
2026-06-09 21:26:52 +0000

    fix: Force Guava dependency resolution to fix conflict
    com.google.guava:listenablefuture:1.0 conflicts with
    com.google.guava:guava:32.1.1-jre. Force guava-android variant
    which includes listenablefuture.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -59,6 +59,13 @@ android {
     }
 }
 
+configurations.all {
+    resolutionStrategy {
+        force("com.google.guava:guava:32.1.1-android")
+        force("com.google.guava:listenablefuture:1.0")
+    }
+}
+
 dependencies {
     implementation(project(":shared"))
 
